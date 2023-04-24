60f86119152c23aa697b46d5911bb433e5b6f07e
fkmr59 <f4k0m6r0@icloud.com>
2023-04-24 10:52:33 +0900

    入力フォームとDBの構成を調整

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
+  attribute :title, :string
+  attribute :category, :integer
+  attribute :content, :text
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_051649) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_013937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "content"
+    t.integer "category"
   end
 
 end
