e7322d81faebf2638481c86bfae05b132dc0f146
fkmr59 <f4k0m6r0@icloud.com>
2023-04-24 16:40:09 +0900

    マイグレーションのremove

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_055118) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_013937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,14 +22,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_055118) do
     t.integer "category", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "blogs_id", null: false
-    t.string "other_user_name", null: false
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["blogs_id"], name: "index_comments_on_blogs_id"
-  end
-
-  add_foreign_key "comments", "blogs", column: "blogs_id"
 end
