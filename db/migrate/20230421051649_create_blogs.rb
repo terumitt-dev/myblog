169e154ccc2106cd4a90c701f5984d9fcc810586
fkmr59 <f4k0m6r0@icloud.com>
2023-04-24 09:43:23 +0900

    rubocopでインデントや記述の修正

--- db/migrate/20230421051649_create_blogs.rb
+++ db/migrate/20230421051649_create_blogs.rb
@@ -1,8 +1,7 @@
+# frozen_string_literal: true
+
 class CreateBlogs < ActiveRecord::Migration[7.0]
   def change
-    create_table :blogs do |t|
-
-      t.timestamps
-    end
+    create_table :blogs, &:timestamps
   end
 end
