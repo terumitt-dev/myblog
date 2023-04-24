dc9b9d46b996db26dec95fcdb9b28991e154a014
fkmr59 <f4k0m6r0@icloud.com>
2023-04-24 10:53:01 +0900

    DBのカラム追加

---
+++ db/migrate/20230424013937_add_columns_to_blogs.rb
@@ -0,0 +1,7 @@
+class AddColumnsToBlogs < ActiveRecord::Migration[7.0]
+  def change
+    add_column :blogs, :title, :string
+    add_column :blogs, :content, :text
+    add_column :blogs, :category, :integer
+  end
+end
