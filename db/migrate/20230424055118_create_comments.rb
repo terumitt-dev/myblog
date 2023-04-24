d0b4f6727420e6fb6f055351e0aad58d73622785
fkmr59 <f4k0m6r0@icloud.com>
2023-04-24 16:36:14 +0900

    コメントのマイグレーションの修正

--- db/migrate/20230424055118_create_comments.rb
+++ db/migrate/20230424055118_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
       t.references :blogs, null: false, foreign_key: true
       t.string :other_user_name, null: false
-      t.text :content
+      t.text :comment
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_055118) do
   create_table "comments", force: :cascade do |t|
     t.bigint "blogs_id", null: false
     t.string "other_user_name", null: false
-    t.text "content"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["blogs_id"], name: "index_comments_on_blogs_id"
