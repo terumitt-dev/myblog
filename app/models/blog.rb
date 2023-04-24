8df9278e8e40c757c1d80f17c8d8cca9d5584dfc
fkmr59 <f4k0m6r0@icloud.com>
2023-04-24 13:01:02 +0900

    カテゴリーの数値によって文字列の出力を調整

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,17 @@ class Blog < ApplicationRecord
   attribute :title, :string
   attribute :category, :integer
   attribute :content, :text
+
+  def category_label
+    case category
+    when 1
+      "Hobby"
+    when 2
+      "Tech"
+    when 3
+      "Other"
+    else
+      "Unknown Category"
+    end
+  end
 end
