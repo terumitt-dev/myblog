0f8b7794a0251350e132cfe03e0125ddfb43bd8e
fkmr59 <f4k0m6r0@icloud.com>
2023-04-25 19:00:44 +0900

    テストを修正してみた

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
+  validates :title, :category, :content, presence: true
   enum category: { uncategorized: 0, hobby: 1, tech: 2, other: 3 }, _default: :uncategorized
 end
