546de830975c0b4a6da825145225954c9ec0714f
fkmr59 <f4k0m6r0@icloud.com>
2023-04-25 11:50:12 +0900

    attributeの記述削除

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
-  attribute :title, :string
-  attribute :category, :integer
-  attribute :content, :text
 
   enum category: { uncategorized: '0', hobby: '1', tech: '2', other: '3' }, _default: '0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_424_013_937) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_013937) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'blogs', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'title', null: false
-    t.text 'content', null: false
-    t.integer 'category', null: false
+  create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "category", null: false
   end
+
 end
