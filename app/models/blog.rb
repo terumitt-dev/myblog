564c55ed2cff71aa5cc52347e9eb168f7ccc6556
fkmr59 <f4k0m6r0@icloud.com>
2023-04-25 12:10:58 +0900

    フォームタグの入力値がデフォで文字列に変換されるためenumと噛み合わない現状が起きている

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
-
-  enum category: { uncategorized: '0', hobby: '1', tech: '2', other: '3' }, _default: '0'
+  enum category: { uncategorized: 0, hobby: 1, tech: 2, other: 3 }, _default: 0
 
   def category_label
     category&.titleize
