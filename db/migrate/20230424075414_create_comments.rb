525b742ee50d4f3ac4dd83a90dd0e5c06d1cc805
fkmr59 <f4k0m6r0@icloud.com>
2023-04-26 11:26:07 +0900

    コメントモデルのカラムを修正

--- db/migrate/20230424075414_create_comments.rb
+++ db/migrate/20230424075414_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :blogs, null: false, foreign_key: true
+      t.references :blog, null: false, foreign_key: true
       t.string :other_user_name, null: false 
       t.text :comment
 
--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_075414) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "blogs_id", null: false
+    t.bigint "blog_id", null: false
     t.string "other_user_name", null: false
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["blogs_id"], name: "index_comments_on_blogs_id"
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
-  add_foreign_key "comments", "blogs", column: "blogs_id"
+  add_foreign_key "comments", "blogs"
 end
