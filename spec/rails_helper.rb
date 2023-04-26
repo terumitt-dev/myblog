e9185645624e00f8fec42e70d194f6eb2b1d092c
fkmr59 <f4k0m6r0@icloud.com>
2023-04-26 15:09:46 +0900

    RSpecの設定ファイルに追記

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,3 +63,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+require 'factory_bot_rails'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
