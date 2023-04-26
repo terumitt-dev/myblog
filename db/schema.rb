11b8049c737c2aea69d9f2b368d6346009df7afe
fkmr59 <f4k0m6r0@icloud.com>
2023-04-26 10:42:35 +0900

    git pull origin main実行後のファイルの内容に違いがないか確認->問題なさそう

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_24_013937) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_075414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_24_013937) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "blogs_id", null: false
-    t.string "other_user_name"
+    t.string "other_user_name", null: false
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
