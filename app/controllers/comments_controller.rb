3d24e85e74dac2ed7662f0eb8e1d7a0a6bd52a8f
fkmr59 <f4k0m6r0@icloud.com>
2023-04-26 14:28:15 +0900

    コメントの編集や削除は行わない方向で考えているので現状はコメントアウト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[show edit update destroy]
+  # before_action :set_comment, only: %i[show edit update destroy]
   before_action :set_blog, only: [:create]
 
   # GET /comments or /comments.json
@@ -14,12 +14,12 @@ class CommentsController < ApplicationController
   # end
 
   # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
+  # def new
+  #   @comment = Comment.new
+  # end
 
   # GET /comments/1/edit
-  def edit; end
+  # def edit; end
 
   # POST /comments or /comments.json
   def create
@@ -37,34 +37,34 @@ class CommentsController < ApplicationController
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to blog_url(@blog), notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @comment.update(comment_params)
+  #       format.html { redirect_to blog_url(@blog), notice: 'Comment was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @comment }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /comments/1 or /comments/1.json
-  def destroy
-    @comment.destroy
+  # def destroy
+  #   @comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to blog_url(@blog), notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to blog_url(@blog), notice: 'Comment was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
+  # def set_comment
+  #   @comment = Comment.find(params[:id])
+  # end
 
   def set_blog
     @blog = Blog.find(params[:blog_id])
