3d1ee90fe659ce44c8760dea87f5542282052b63
fkmr59 <f4k0m6r0@icloud.com>
2023-04-27 15:29:41 +0900

    モデルのテストを記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  it "other_user_nameがない場合は無効であること" do
+    comment = Comment.new(other_user_name: nil)
+    expect(comment).not_to be_valid
+  end
+  
+  it "blog_idがない場合は無効であること" do
+    comment = Comment.new(blog_id: nil)
+    expect(comment).not_to be_valid
+  end
+  
+  it "blog_idとother_user_nameがある場合は有効であること" do
+    blog = Blog.create(title: "test", content: "test")
+      comment = blog.comments.create(other_user_name: "test")
+      expect(comment).to be_valid
+    end
+  end
