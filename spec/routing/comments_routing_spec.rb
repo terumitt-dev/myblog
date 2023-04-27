b0a49615d1830bacedf79a6368e4cc5d282ff921
fkmr59 <f4k0m6r0@icloud.com>
2023-04-27 14:52:10 +0900

    commentのルートをパスできた

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -2,38 +2,37 @@
 
 require 'rails_helper'
 
-RSpec.describe CommentsController, type: :routing do
-  describe 'routing' do
-    it 'routes to #index' do
-      expect(get: '/blogs/:blog_id/comments').to route_to('comments#index')
-    end
+RSpec.describe "routing to comments", type: :routing do
+  it "routes GET /blogs/:blog_id/comments to comments#index" do
+    expect(get: "/blogs/1/comments").to route_to(controller: "comments", action: "index", blog_id: "1")
+  end
 
-    it 'routes to #new' do
-      expect(get: '/blogs/1/comments/new').to route_to('comments#new')
-    end
+  it "routes GET /blogs/:blog_id/comments/new to comments#new" do
+    expect(get: "/blogs/1/comments/new").to route_to(controller: "comments", action: "new", blog_id: "1")
+  end
 
-    it 'routes to #show' do
-      expect(get: '/blogs/1/comments/1').to route_to('comments#show', id: '1')
-    end
+  it "routes GET /blogs/:blog_id/comments/:id to comments#show" do
+    expect(get: "/blogs/1/comments/1").to route_to(controller: "comments", action: "show", blog_id: "1", id: "1")
+  end
 
-    it 'routes to #edit' do
-      expect(get: '/blogs/1/comments/1/edit').to route_to('comments#edit', id: '1')
-    end
+  it "routes GET /blogs/:blog_id/comments/:id/edit to comments#edit" do
+    expect(get: "/blogs/1/comments/1/edit").to route_to(controller: "comments", action: "edit", blog_id: "1", id: "1")
+  end
 
-    it 'routes to #create' do
-      expect(post: '/blogs/1/comments').to route_to('comments#create')
-    end
+  it "routes POST /blogs/:blog_id/comments to comments#create" do
+    expect(post: "/blogs/1/comments").to route_to(controller: "comments", action: "create", blog_id: "1")
+  end
 
-    it 'routes to #update via PUT' do
-      expect(put: '/blogs/1/comments/1').to route_to('comments#update', id: '1')
-    end
+  it "routes PUT /blogs/:blog_id/comments/:id to comments#update" do
+    expect(put: "/blogs/1/comments/1").to route_to(controller: "comments", action: "update", blog_id: "1", id: "1")
+  end
 
-    it 'routes to #update via PATCH' do
-      expect(patch: '/blogs/1/comments/1').to route_to('comments#update', id: '1')
-    end
+  it "routes PATCH /blogs/:blog_id/comments/:id to comments#update" do
+    expect(patch: "/blogs/1/comments/1").to route_to(controller: "comments", action: "update", blog_id: "1", id: "1")
+  end
 
-    it 'routes to #destroy' do
-      expect(delete: '/blogs/1/comments/1').to route_to('comments#destroy', id: '1')
-    end
+  it "routes DELETE /blogs/:blog_id/comments/:id to comments#destroy" do
+    expect(delete: "/blogs/1/comments/1").to route_to(controller: "comments", action: "destroy", blog_id: "1", id: "1")
   end
 end
+
