e894bdc7516bd5e5ebd3eda342498ac8643bd1c3
fkmr59 <f4k0m6r0@icloud.com>
2023-05-01 11:32:42 +0900

    ブログ記事削除に合わせてコメントの自動削除

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, :category, :content, presence: true
   enum category: { uncategorized: 0, hobby: 1, tech: 2, other: 3 }, _default: :uncategorized
+
+  def delete_comments
+    comments.destroy_all
+  end
 end
