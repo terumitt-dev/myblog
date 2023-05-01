e5c3a197a018ce12f2b6382bc6e233a7cf3751cf
fkmr59 <f4k0m6r0@icloud.com>
2023-05-01 17:06:26 +0900

    エクスポートしたファイルの内容をインポートできるようにする

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'nokogiri'
 
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy]
@@ -56,6 +57,23 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+
+  def data_port
+    file = params[:import_file]
+    doc = Nokogiri::HTML(file.read)
+    title = doc.search('title').text
+    content = doc.search('body').text
+
+    @blog = Blog.new(title: title, content: content)
+
+    if @blog.save
+      redirect_to blogs_path, notice: 'Blog was successfully created.'
+    else
+      render :new
+    end
+  end
+  
 
   private
 
