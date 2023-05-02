cf99808e0e325f0cbcf6f5a30283cb981fddfd3f
fkmr59 <f4k0m6r0@icloud.com>
2023-05-02 11:16:38 +0900

    コメントモデルのテスト記述を修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,19 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  it 'other_user_nameがない場合は無効であること' do
-    comment = Comment.new(other_user_name: nil)
-    expect(comment).not_to be_valid
+  describe 'validations' do
+    
+    let(:blog) { Blog.create(title: 'Test', category: :hobby, content: 'test content') }
+
+    it 'other_user_nameがない場合は無効であること' do
+      comment = blog.comments.build(other_user_name: nil)
+      expect(comment).to_not be_valid
+    end
+
+    it 'blogがない場合は無効であること' do
+      comment = Comment.new(other_user_name: 'John')
+      expect(comment).to_not be_valid
+    end
   end
 
-  it 'blog_idがない場合は無効であること' do
-    comment = Comment.new(blog_id: nil)
-    expect(comment).not_to be_valid
-  end
-
-  it 'blog_idとother_user_nameがある場合は有効であること' do
-    blog = Blog.create(title: 'test', content: 'test')
-    comment = blog.comments.create(other_user_name: 'test')
-    expect(comment).to be_valid
+  describe 'associations' do
+    it 'Blogとのアソシエーション' do
+      expect(Comment.reflect_on_association(:blog).macro).to eq :belongs_to
+    end
   end
 end
