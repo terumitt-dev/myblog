28577e482f81b34c4084937ac8659a95084c710c
fkmr59 <f4k0m6r0@icloud.com>
2023-05-08 16:19:40 +0900

    Revert "エラー解消できないので、revertを実行する"
    This reverts commit cf088789e258c51b374c309ac6567db0ee0aabf5.

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class HomeController < ApplicationController
+class Admin::HomeController < ApplicationController
   before_action :authenticate_admin!
 
   def index
