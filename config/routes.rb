2221777dfbdf631ae44432567b7398ca90b92f14
fkmr59 <f4k0m6r0@icloud.com>
2023-05-08 10:31:00 +0900

    adminコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'home/index'
+  end
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
