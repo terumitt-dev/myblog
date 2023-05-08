55fe6b8318260576e9a163c1cab6576d18c897d6
fkmr59 <f4k0m6r0@icloud.com>
2023-05-08 18:39:19 +0900

    rubocopによる体裁の修正

--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Admins", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admins/index"
+RSpec.describe 'Admins', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/admins/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
