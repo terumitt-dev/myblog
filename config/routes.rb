92298764060d3b1a3b8fe1c2e5573872b46528d4
fkmr59 <f4k0m6r0@icloud.com>
2023-05-08 10:51:25 +0900

    routeの記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :blogs
+  
   namespace :admin do
     get 'home/index'
   end
-  resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
