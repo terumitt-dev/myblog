8c38eb911fbd8b7aa3a38cc7a076dd6a64caede9
fkmr59 <f4k0m6r0@icloud.com>
2023-05-08 16:54:47 +0900

    エラーの根本的な状態がわからないのでrevert

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
-module Admin
+# module Admin
+#   class HomeController < ApplicationController
+#     before_action :authenticate_admin!
+
+#     def index
+#       @blogs = Blog.all
+#     end
+#   end
+# end
+
+module AdminArea
   class HomeController < ApplicationController
     before_action :authenticate_admin!
 
