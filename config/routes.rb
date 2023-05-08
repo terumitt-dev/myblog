54b09c1e5b98dada151f44e170dfab09bd04b01f
fkmr59 <f4k0m6r0@icloud.com>
2023-05-08 18:38:23 +0900

    routesの記述を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'admins/index'
+  # get 'admins/index'
+  # devise_for :admins
+
   devise_for :admins
+  namespace :admin do
+    resources :dashboard, only: [:index]
+  end
+
   resources :blogs do
     resources :comments
   end
