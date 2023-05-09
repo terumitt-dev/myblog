ff942220c96633b3f404254e5007b93b2f00d819
fkmr59 <f4k0m6r0@icloud.com>
2023-05-09 14:22:28 +0900

    削除後のリダイレクトの記述を修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -45,7 +45,7 @@ class BlogsController < ApplicationController
   # DELETE /blogs/1 or /blogs/1.json
   def destroy
     @blog.destroy
-    redirect_to blogs_url, notice: 'Blog was successfully destroyed.'
+    redirect_to admin_root_url, notice: 'Blog was successfully destroyed.'
   end
 
   private
