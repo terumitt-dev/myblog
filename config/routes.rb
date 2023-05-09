7dc71d42437a65e22e0d8510ef260b72387b103d
fkmr59 <f4k0m6r0@icloud.com>
2023-05-09 16:28:36 +0900

    URLの直接入力で未定義のPATHへのアクセスはrootに回帰するように記述

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get '*path', to: redirect('/')
   root 'blogs#index'
 end
