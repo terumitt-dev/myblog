533de1656a7049e8ad189a3d78c9c02019362a1d
fkmr59 <f4k0m6r0@icloud.com>
2023-05-10 12:18:53 +0900

    spec/requests/admins_spec.rbのテストを試行錯誤中

--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -2,11 +2,34 @@
 
 require 'rails_helper'
 
+RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :request
+end
+
 RSpec.describe 'Admins', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/admins/index'
-      expect(response).to have_http_status(:success)
+    context 'ログイン済みの場合' do
+      let!(:admin) { FactoryBot.create(:admin) }
+      let!(:blog) { FactoryBot.create(:blog) }
+
+      before do
+        sign_in admin
+      end
+    
+      it 'indexテンプレートを表示すること' do
+        get admin_root_url
+        expect(response).to be_successful
+      end
+    
+      it 'assigns @blogs' do
+        get admin_root_url
+        expect(assigns(:blogs)).to eq([blog])
+      end
+    
+      it 'assigns @blog' do
+        get admin_root_url
+        expect(assigns(:blog)).to be_a_new(Blog)
+      end
     end
   end
 end
