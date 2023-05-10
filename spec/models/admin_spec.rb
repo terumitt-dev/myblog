3fee81930a8bb9cccd88c757b5431ee6c0eafa48
fkmr59 <f4k0m6r0@icloud.com>
2023-05-10 10:24:03 +0900

    adminモデルのテストを記述

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'emailがない場合無効である' do
+      admin = Admin.new(email: nil, password: 'password')
+      expect(admin).not_to be_valid
+    end
+
+    it 'passwordがない場合無効である' do
+      admin = Admin.new(email: 'admin@example.com', password: nil)
+      expect(admin).not_to be_valid
+    end
+
+    it '短すぎるpasswordの場合無効である' do
+      admin = Admin.new(email: 'admin@example.com', password: 'short')
+      expect(admin).not_to be_valid
+    end
+
+    it '6文字以上のpasswordの場合有効である' do
+      admin = Admin.new(email: 'admin@example.com', password: 'long_enough_password')
+      expect(admin).to be_valid
+    end
+  end
 end
