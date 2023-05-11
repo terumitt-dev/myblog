8660cb33c9fb38ed83429b25a460b5714cd7fbe1
fkmr59 <f4k0m6r0@icloud.com>
2023-05-11 11:11:32 +0900

    一度元の記述に修正

--- spec/routing/blogs_routing_spec.rb
+++ spec/routing/blogs_routing_spec.rb
@@ -2,91 +2,36 @@
 
 require 'rails_helper'
 
-RSpec.configure do |config|
-  config.include Devise::Test::IntegrationHelpers, type: :routing
-end
-
 RSpec.describe 'routes for blogs', type: :routing do
-  describe 'routing' do
-    context '管理者ユーザーの場合' do
-      let!(:admin) { FactoryBot.create(:admin) }
-      before do
-        sign_in admin 
-      end
-        
-      it 'routes to #index' do
-        expect(get: '/blogs').to route_to('blogs#index')
-      end
+  it 'routes to #index' do
+    expect(get: '/blogs').to route_to('blogs#index')
+  end
 
-      it 'routes to #new' do
-        expect(get: '/blogs/new').to route_to('blogs#new')
-      end
+  it 'routes to #new' do
+    expect(get: '/blogs/new').to route_to('blogs#new')
+  end
 
-      it 'routes to #show' do
-        expect(get: '/blogs/1').to route_to('blogs#show', id: '1')
-      end
+  it 'routes to #show' do
+    expect(get: '/blogs/1').to route_to('blogs#show', id: '1')
+  end
 
-      it 'routes to #edit' do
-        expect(get: '/blogs/1/edit').to route_to('blogs#edit', id: '1')
-      end
+  it 'routes to #edit' do
+    expect(get: '/blogs/1/edit').to route_to('blogs#edit', id: '1')
+  end
 
-      it 'routes to #create' do
-        expect(post: '/blogs').to route_to('blogs#create')
-      end
+  it 'routes to #create' do
+    expect(post: '/blogs').to route_to('blogs#create')
+  end
 
-      it 'routes to #update via PUT' do
-        expect(put: '/blogs/1').to route_to('blogs#update', id: '1')
-      end
+  it 'routes to #update via PUT' do
+    expect(put: '/blogs/1').to route_to('blogs#update', id: '1')
+  end
 
-      it 'routes to #update via PATCH' do
-        expect(patch: '/blogs/1').to route_to('blogs#update', id: '1')
-      end
+  it 'routes to #update via PATCH' do
+    expect(patch: '/blogs/1').to route_to('blogs#update', id: '1')
+  end
 
-      it 'routes to #destroy' do
-        expect(delete: '/blogs/1').to route_to('blogs#destroy', id: '1')
-      end
-
-      it 'routes to #index as admin_root' do
-        expect(get: '/').to route_to('admins#index')
-      end
-    end
-
-    context '一般ユーザーの場合' do
-      it 'routes to #index' do
-        expect(get: '/blogs').to route_to('blogs#index')
-      end
-
-      it 'routes to #show' do
-        expect(get: '/blogs/1').to route_to('blogs#show', id: '1')
-      end
-
-      it 'does not route to #new' do
-        expect(get: '/blogs/new').not_to route_to('blogs#new')
-      end
-
-      it 'does not route to #edit' do
-        expect(get: '/blogs/1/edit').not_to route_to('blogs#edit', id: '1')
-      end
-
-      it 'does not route to #create' do
-        expect(post: '/blogs').not_to route_to('blogs#create')
-      end
-
-      it 'does not route to #update via PUT' do
-        expect(put: '/blogs/1').not_to route_to('blogs#update', id: '1')
-      end
-
-      it 'does not route to #update via PATCH' do
-        expect(patch: '/blogs/1').not_to route_to('blogs#update', id: '1')
-      end
-
-      it 'does not route to #destroy' do
-        expect(delete: '/blogs/1').not_to route_to('blogs#destroy', id: '1')
-      end
-
-      it 'routes to #index as root' do
-        expect(get: '/').to route_to('blogs#index')
-      end
-    end
+  it 'routes to #destroy' do
+    expect(delete: '/blogs/1').to route_to('blogs#destroy', id: '1')
   end
 end
