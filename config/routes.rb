54337e5767ba47bca9f961a9a39c460cc325d718
fkmr59 <f4k0m6r0@icloud.com>
2023-05-12 12:06:39 +0900

    自動ツイートされない

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   resources :blogs do
     resources :comments
+    post 'tweet', on: :member
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
