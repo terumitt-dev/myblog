60b88bee44b0d2d236778a34f4aa90a72213b17c
fkmr59 <f4k0m6r0@icloud.com>
2023-05-15 18:18:28 +0900

    コントローラーの記述を変更

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -2,17 +2,13 @@
 
 module Admins
   class RegistrationsController < Devise::RegistrationsController
-    before_action :one_user_registered?, only: %i[new create]
+    before_action :check_registration_limit, only: %i[new create]
 
     protected
 
-    def one_user_registered?
-      if Admin.count == 1 && admin_signed_in?
-        redirect_to root_path
-      else
-        if Admin.count == 1
-          redirect_to new_admin_session_path
-        end
+    def check_registration_limit
+      if Admin.count >= 1
+        redirect_to root_path, alert: 'Only one admin allowed'
       end
     end
   end
