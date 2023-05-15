8ef0826fbc681e72e9c7b114b6fd5c23fd43341f
fkmr59 <f4k0m6r0@icloud.com>
2023-05-15 16:26:38 +0900

    deviseのviweを生成

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -9,8 +9,10 @@ module Admins
     def one_user_registered?
       if Admin.count == 1 && admin_signed_in?
         redirect_to root_path
-      elsif Admin.count == 1
-        redirect_to new_admin_session_path
+      else
+        if Admin.count == 1
+          redirect_to new_admin_session_path
+        end
       end
     end
   end
