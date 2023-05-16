66b3457ce58b8acb85b2ab0962db0d7aebc60a04
fkmr59 <f4k0m6r0@icloud.com>
2023-05-16 11:11:00 +0900

    体裁の修正とsignupのリンク表示

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -20,7 +20,8 @@ RSpec.describe Admin, type: :model do
     end
 
     it '6文字以上のpasswordの場合有効である' do
-      admin = Admin.new(email: 'admin@example.com', password: 'long_enough_password', password_confirmation: 'long_enough_password')
+      admin = Admin.new(email: 'admin@example.com', password: 'long_enough_password',
+                        password_confirmation: 'long_enough_password')
       expect(admin).to be_valid
     end
   end
@@ -28,17 +29,19 @@ RSpec.describe Admin, type: :model do
   describe '#only_one_admin_allowed' do
     context '既に管理者が1人いる場合' do
       let!(:admin) { FactoryBot.create(:admin) }
-  
+
       it 'エラーメッセージを追加する' do
-        admin = Admin.new(email: 'demo@example.com', password: 'long_enough_password', password_confirmation: 'long_enough_password')
+        admin = Admin.new(email: 'demo@example.com', password: 'long_enough_password',
+                          password_confirmation: 'long_enough_password')
         admin.valid?
         expect(admin.errors[:base]).to include('Only one admin allowed')
       end
     end
-  
+
     context '管理者が設定されていない場合' do
       it 'エラーメッセージを追加しない' do
-        admin = Admin.new(email: 'test@example.com', password: 'long_enough_password', password_confirmation: 'long_enough_password')
+        admin = Admin.new(email: 'test@example.com', password: 'long_enough_password',
+                          password_confirmation: 'long_enough_password')
         admin.valid?
         expect(admin.errors[:base]).to be_empty
       end
