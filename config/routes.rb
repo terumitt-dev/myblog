95d5db3e897ce74f2a663723d912776e2e886118
fkmr59 <f4k0m6r0@icloud.com>
2023-05-16 09:43:48 +0900

    ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # 仕様変更で削除する可能性大↓
-  # devise_for :admins, controllers: { registrations: 'admins/registrations' }, path: 'admins'
-
   devise_for :admins
 
   authenticated :admin do
