2cf96ca4cc6c394e6567c7cd914e7e78ff9389e3
fkmr59 <f4k0m6r0@icloud.com>
2023-05-17 18:16:36 +0900

    元に戻す

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,4 @@
 //= link_tree ../images
 // = link_directory ../stylesheets/ .css
-//= link_directory ../stylesheets/blogs .css
-//= link_directory ../stylesheets/comments .css
-//= link_directory ../stylesheets/layouts .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
