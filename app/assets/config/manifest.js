55de1b4ee190850c079944532798e768aacef710
fkmr59 <f4k0m6r0@icloud.com>
2023-05-17 17:49:50 +0900

    app/assets/config/manifest.jsの記述を変更

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,7 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+// = link_directory ../stylesheets/ .css
+//= link_directory ../stylesheets/blogs .css
+//= link_directory ../stylesheets/comments .css
+//= link_directory ../stylesheets/layouts .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link layouts/application.css
