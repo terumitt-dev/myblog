9821ca80caa31539ea32960b85fc33ef8c482b47
fkmr59 <f4k0m6r0@icloud.com>
2023-05-18 17:09:38 +0900

    env設定を移行

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'smtp.gmail.com',
-    user_name: ENV['GOOGLE_MAIL_ADDRESS'],
-    password: ENV['GOOGLE_MAILER_PASSWORD'],
+    user_name: Rails.application.credentials.gmail[:google_mail_address],
+    password: Rails.application.credentials.gmail[:google_mailer_password],
     authentication: 'login'
   }
 end
