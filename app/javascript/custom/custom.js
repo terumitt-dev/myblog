4957fe6ea77747638627a8ff53844ec96ebdd8d3
fkmr59 <f4k0m6r0@icloud.com>
2023-05-30 17:06:12 +0900

    この記述でも期待する挙動と少し違う

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   const noticeElement = document.getElementById("notice");
   const alertElement = document.getElementById("alert");
 
-  if (!(noticeElement.textContent.trim() === "" || alertElement.textContent.trim() === "")) {
-  return;
+  if (noticeElement.textContent.trim() === "" || alertElement.textContent.trim() === "") {
+    return;
   }
   
   setTimeout(function() {
