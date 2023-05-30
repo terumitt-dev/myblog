30978305ac24548230176facf85ba1dfcde86de3
fkmr59 <f4k0m6r0@icloud.com>
2023-05-30 15:28:36 +0900

    この記述でも期待する挙動と違う

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,37 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   const noticeElement = document.getElementById("notice");
-//   const alertElement = document.getElementById("alert");
-
-//   if (noticeElement.innerHTML.trim() === "" || alertElement.innerHTML.trim() === "") {
-//     setTimeout(function() {
-//       noticeElement.style.display = "none";
-//       alertElement.style.display = "none";
-//       location.reload();
-//     }, 5000);
-//   }
-// });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const noticeElement = document.getElementById("notice");
-//   const alertElement = document.getElementById("alert");
-
-//   if (noticeElement.innerHTML.trim() !== null || alertElement.innerHTML.trim() !== null) {
-//     setTimeout(function() {
-//       noticeElement.style.display = "none";
-//       alertElement.style.display = "none";
-//       location.reload();
-//     }, 5000);
-//   }
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
   const noticeElement = document.getElementById("notice");
   const alertElement = document.getElementById("alert");
 
-  if (noticeElement.innerHTML.trim() === null && alertElement.innerHTML.trim() === null) {
-    return;
+  if (!(noticeElement.textContent.trim() === "" || alertElement.textContent.trim() === "")) {
+  return;
   }
-
+  
   setTimeout(function() {
     noticeElement.style.display = "none";
     alertElement.style.display = "none";
