f8c1eb361cb0deddad7b3901d3bbad157284f1ac
fkmr59 <f4k0m6r0@icloud.com>
2023-05-31 12:57:43 +0900

    エンジンテンプレートによる影響でテキストをJSで取得することができないみたい

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -2,13 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
   const noticeElement = document.getElementById("notice");
   const alertElement = document.getElementById("alert");
 
-  if (noticeElement.textContent.trim() === "" || alertElement.textContent.trim() === "") {
-    return;
+  if (noticeElement.textContent.trim() !== "" || alertElement.textContent.trim() !== "") {
+    setTimeout(function() {
+      location.reload();
+    }, 5000);
   }
-  
-  setTimeout(function() {
-    noticeElement.style.display = "none";
-    alertElement.style.display = "none";
-    location.reload();
-  }, 5000);
 });
