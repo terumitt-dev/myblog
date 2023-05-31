1082e929076ec85ff350b8c3fb83068e240bc7d7
fkmr59 <f4k0m6r0@icloud.com>
2023-05-31 16:28:50 +0900

    うまくいかない

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const noticeElement = document.getElementById("notice");
-  const alertElement = document.getElementById("alert");
+  const noticeElement = document.getElementById("notice").dataset.notice;
+  const alertElement = document.getElementById("alert").dataset.alert;
 
   if (noticeElement.textContent.trim() !== "" || alertElement.textContent.trim() !== "") {
+    noticeElement.style.opacity = "1";
+    alertElement.style.opacity = "1";
+
     setTimeout(function() {
-      location.reload();
-    }, 5000);
+      noticeElement.style.transition = "opacity 3s";
+      alertElement.style.transition = "opacity 3s";
+      noticeElement.style.opacity = "0";
+      alertElement.style.opacity = "0";
+    }, 2000);
   }
 });
