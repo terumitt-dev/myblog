9e6314c9c175390a4e9f35f2bfb4abf99d5027f1
fkmr59 <f4k0m6r0@icloud.com>
2023-09-01 00:57:41 +0900

    jsとviewの改善

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,27 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const noticeElements = document.querySelectorAll(".notice");
-  const alertElements = document.querySelectorAll(".alert");
+const noticeElements = document.querySelectorAll(".notice");
+const alertElements = document.querySelectorAll(".alert");
 
-  console.log("noticeElements:", noticeElements);
-  console.log("alertElements:", alertElements);
-
-  noticeElements.forEach(function(noticeElement) {
-    const noticeMessage = noticeElement.textContent.trim();
-    console.log("noticeMessage:", noticeMessage);
-
-    setTimeout(function() {
-      noticeElement.style.transition = "opacity 3s";
-      noticeElement.style.opacity = "0";
-    }, 2000);
-  });
-
-  alertElements.forEach(function(alertElement) {
-    const alertMessage = alertElement.textContent.trim();
-    console.log("alertMessage:", alertMessage);
-
-    setTimeout(function() {
-      alertElement.style.transition = "opacity 3s";
-      alertElement.style.opacity = "0";
-    }, 2000);
-  });
-});
+console.log(noticeElements);
+console.log(alertElements);
