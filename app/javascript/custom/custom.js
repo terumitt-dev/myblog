473e36a06b3a5b927349a01c6f41204f96f50ae8
fkmr59 <f4k0m6r0@icloud.com>
2023-09-02 01:51:48 +0900

    JSの記述を修正

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,5 +1,18 @@
-const noticeElements = document.querySelectorAll(".notice");
-const alertElements = document.querySelectorAll(".alert");
+const noticeElements = document.getElementsByClassName("notice");
+const alertElements = document.getElementsByClassName("alert");
 
 console.log(noticeElements);
 console.log(alertElements);
+
+// noticeElementsWithTextContentとalertElementsWithTextContentの要素数をチェックする
+if (noticeElements.textContent !== "") {
+  setTimeout(() => {
+      element.style.opacity = 0;
+  }, 3000);
+}
+
+if (alertElements.textContent !== "") {
+  setTimeout(() => {
+      element.style.opacity = 0;
+  }, 5000);
+}
