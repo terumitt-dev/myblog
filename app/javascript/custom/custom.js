da30d29608ec5ede8f18426eb014277efd9770ec
fkmr59 <f4k0m6r0@icloud.com>
2023-10-08 01:25:36 +0900

    値が取れない理由がわかった

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -3,16 +3,3 @@ const alertElements = document.getElementsByClassName("alert");
 
 console.log(noticeElements);
 console.log(alertElements);
-
-noticeElementsWithTextContentとalertElementsWithTextContentの要素数をチェックする
-if (noticeElements.textContent !== "") {
-  setTimeout(() => {
-      element.style.opacity = 0;
-  }, 3000);
-}
-
-if (alertElements.textContent !== "") {
-  setTimeout(() => {
-      element.style.opacity = 0;
-  }, 5000);
-}
