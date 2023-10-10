caf4317197d52f47d3a26c7c4407eada9ba9cc50
fkmr59 <f4k0m6r0@icloud.com>
2023-10-11 00:19:34 +0900

    JSでフラッシュメッセージを取得できないかテスト

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -3,3 +3,9 @@ const alertElements = document.getElementsByClassName("alert");
 
 console.log(noticeElements);
 console.log(alertElements);
+
+const noticeText = noticeElements[0].textContent;
+const alertText = alertElements[0].textContent;
+
+console.log(noticeText);
+console.log(alertText);
