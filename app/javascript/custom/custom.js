1fbb8bcff86b3bbbc3a74ffeb959de0e20036832
fkmr59 <f4k0m6r0@icloud.com>
2023-10-25 23:40:11 +0900

    メッセージを消すことができるようになった

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -4,8 +4,16 @@ const alertElements = document.getElementsByClassName("alert");
 console.log(noticeElements);
 console.log(alertElements);
 
-const noticeText = noticeElements[0].textContent;
-const alertText = alertElements[0].textContent;
+function fadeout() {
+  noticeElements[0].style.opacity = 1;
+  noticeElements[0].style.transition = "opacity 3s linear";
+  noticeElements[0].style.opacity = 0;
+  noticeElements[0].style.display = "none";
 
-console.log(noticeText);
-console.log(alertText);
+  alertElements[0].style.opacity = 1;
+  alertElements[0].style.transition = "opacity 3s linear";
+  alertElements[0].style.opacity = 0;
+  alertElements[0].style.display = "none";
+}
+
+setTimeout(fadeout, 5000);
