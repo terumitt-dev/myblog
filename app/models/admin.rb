ef271a0f2e0341ce221f9289adb54d51d666cf1e
terumitt <f4k0m6r0@icloud.com>
2025-03-12 00:36:58 +0900

    パスワードリセット機能の復元とrubocopによる体裁修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
+# class Admin < ApplicationRecord
+#   # Include default devise modules. Others available are:
+#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+#   devise :database_authenticatable, :registerable,
+#          :recoverable, :rememberable, :validatable
+
+#   validate :only_one_admin_allowed
+
+#   private
+
+#   def only_one_admin_allowed
+#     return if Admin.count.zero?
+
+#     errors.add(:base, 'Only one admin allowed')
+#   end
+# end
+
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validate :only_one_admin_allowed
+  validate :only_one_admin_allowed, on: :create
 
   private
 
