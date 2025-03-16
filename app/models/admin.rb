da2e3cfd16b1ad1313ec1f4add7fb72bc811e75c
terumitt <f4k0m6r0@icloud.com>
2025-03-17 00:28:23 +0900

    元の記述を削除

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 
-# class Admin < ApplicationRecord
-#   # Include default devise modules. Others available are:
-#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-#   devise :database_authenticatable, :registerable,
-#          :recoverable, :rememberable, :validatable
-
-#   validate :only_one_admin_allowed
-
-#   private
-
-#   def only_one_admin_allowed
-#     return if Admin.count.zero?
-
-#     errors.add(:base, 'Only one admin allowed')
-#   end
-# end
-
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
