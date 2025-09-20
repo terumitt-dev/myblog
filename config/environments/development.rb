0bf445c902d49bd3592b3a026824173c3ee72ee2
terumitt <f4k0m6r0@icloud.com>
2025-09-21 00:48:17 +0900

    Rails8系へのアップデートで削除してしまったメール設定を復旧

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,17 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+       address: 'smtp.gmail.com',
+       port: 587,
+       domain: 'smtp.gmail.com',
+       user_name: Rails.application.credentials.gmail[:google_mail_address],
+       password: Rails.application.credentials.gmail[:google_mailer_password],
+       authentication: 'login'
+    }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
