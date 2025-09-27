05bae900737cb619832c1c38c4f8756316e85a49
terumitt <f4k0m6r0@icloud.com>
2025-09-27 22:58:04 +0900

    テストの保存漏れ

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -33,4 +33,36 @@ RSpec.describe Blog, type: :model do
       end
     end
   end
+
+  describe '.import_from_mt' do
+    it '正しいMTファイルでブログが作成されること' do
+      temp_file = Tempfile.new(['sample', '.mt'])
+      temp_file.write("AUTHOR: test\nTITLE: サンプルブログ\nDATE: 12/17/2024 19:00:00\nBODY:\n本文です\n-----\n")
+      temp_file.rewind
+
+      expect {
+        Blog.import_from_mt(temp_file)
+      }.to change(Blog, :count).by(1)
+
+      blog = Blog.last
+      expect(blog.title).to eq 'サンプルブログ'
+      expect(blog.content).to eq '本文です'
+
+      temp_file.close
+      temp_file.unlink
+    end
+
+    it '無効なMTファイルではブログが作成されないこと' do
+      temp_file = Tempfile.new(['invalid', '.txt'])
+      temp_file.write("無効な内容")
+      temp_file.rewind
+
+      expect {
+        Blog.import_from_mt(temp_file)
+      }.to_not change(Blog, :count)
+
+      temp_file.close
+      temp_file.unlink
+    end
+  end
 end
